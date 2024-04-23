b71697e0974ebf99091f85a8f35d56c711ec69bc
Chijioke29 <cnkwoch@okstate.edu>
2024-04-23 12:19:39 -0500

    modified utils (checkpoint)

--- utils.py
+++ utils.py
@@ -166,7 +166,7 @@ def load_checkpoint(model, optimizer, folder_dir, filename):
         model_path), "The model file \"{0}\" doesn't exist.".format(filename)
 
     # Load the stored model parameters to the model instance
-    checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
+    checkpoint = torch.load(model_path)
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
     epoch = checkpoint['epoch']
